f02e85a060029cd7e497120a266a715c60a34b46
Justin Phan <justinphan300000@yahoo.com>
2023-10-23 18:47:51 -0400

    Created CommonConfig and PeerInfo.java files to read the corresponding .cfg files

---
+++ src/peer/CommonConfig.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.nio.file.*;
+
+// Reads key value pairs from file and store into a map
+public class CommonConfig {
+
+    // Create a Map named properties to store config data as key value pair
+    private final Map<String, String> properties = new HashMap<>();
+
+    // Reads config data from file
+    public CommonConfig(String filename) throws IOException {
+
+        // Reads all lines
+        List<String> lines = Files.readAllLines(Paths.get(filename));
+
+        // Adds key into parts[0] and value into parts[1] in the lines list
+        for (String line : lines) {
+            String[] parts = line.split(" ");
+            properties.put(parts[0], parts[1]);
+        }
+    }
+
+    // Return value of config based on the key
+    public String getProperty(String key) {
+        return properties.get(key);
+
+    }
+}
---
+++ src/peer/PeerInfo.java
@@ -0,0 +1,55 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class PeerInfo {
+
+    private int listeningPortNumber;
+    private int peerID;
+    private String hostName;
+    private boolean hasFile;
+
+    // Constructor that initializes values provided in parameter
+    public PeerInfo(int peerID, String hostName, int listeningPortNumber, boolean hasFile) {
+
+        this.peerID = peerID;
+        this.hostName = hostName;
+        this.listeningPortNumber = listeningPortNumber;
+        this.hasFile = hasFile;
+
+    }
+
+    // Getters that return the value of the constructor above
+    public int getListeningPortNumber() {
+        return listeningPort;
+    }
+
+    public int getPeerID() {
+        return peerID;
+    }
+
+    public int getHasFile() {
+        return hasFile;
+    }
+
+    public int getHostName() {
+        return hostName;
+    }
+
+    // Reads data from a file, having each line be information about a peer and constructs list of PeerInfo objects from that data.
+    public static List<PeerInfo> loadFromFile(String filename) throws IOException {
+
+        List<PeerInfo> peers = new ArrayList<>();
+        List<String> lines = Files.readAllLines(Paths.get(filename));
+
+        // Goes through each line in the list, creating new PeerInfo object using values from parts array and adds to peers list
+        for(String line : lines) {
+            String[] parts = line.split(" ");
+             peers.add(new PeerInfo(Integer.parseInt(parts[0]), parts[1], Integer.parseInt(parts[2]), parts[3].equals("1")));
+        }
+
+        return peers;
+
+    }
+}
